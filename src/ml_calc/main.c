94d425172009fc4a245f2914afa92d3ea9202447
Lukas Vrabec <lukas.vrabec.svk@gmail.com>
2013-04-20 22:04:09 +0200

    Revert "prva verzia gui kalkulacky (nejde kompilovat -- riesim)"
    This reverts commit 931f9feec233da4c82b0d82c7da8c06603b5a306.

--- src/ml_calc/main.c
+++ src/ml_calc/main.c
@@ -1,19 +1,1 @@
 
-#include <gtk/gtk.h>
-#include <glade/glade.h>
-
-int main(int argc, char *argv[])
-{
-        gtk_init(&argc, &argv);
-
-        GladeXML *xml = glade_xml_new("calc.glade", NULL, NULL);
-
-        if (xml == NULL) {
-                g_print("glade_xml_new error\n");
-                return 1;
-        }
-
-        gtk_main();
-
-        return 0;
-}
