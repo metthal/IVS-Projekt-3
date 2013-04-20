d5add7d91fea637b2fa6aeeaf74818bc3f7dc798
Lukas Vrabec <lukas.vrabec.svk@gmail.com>
2013-04-20 22:15:04 +0200

    snaha o prepojenie calc.glade s main.c

--- src/ml_calc/main.c
+++ src/ml_calc/main.c
@@ -1,1 +1,18 @@
+#include <gtk/gtk.h>
+#include <glade/glade.h>
 
+int main(int argc, char *argv[])
+{
+        gtk_init(&argc, &argv);
+
+        GladeXML *xml = glade_xml_new("demo.glade", NULL, NULL);
+
+        if (xml == NULL) {
+                g_print("glade_xml_new error\n");
+                return 1;
+        }
+
+        gtk_main();
+
+        return 0;
+}
