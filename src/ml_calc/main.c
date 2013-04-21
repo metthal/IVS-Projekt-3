d3d71309f4eae87592734b1de6f64d159cb6012c
Lukas Vrabec <lukas.vrabec.svk@gmail.com>
2013-04-21 13:11:32 +0200

    uprava main uz kompilujeme

--- src/ml_calc/main.c
+++ src/ml_calc/main.c
@@ -1,18 +1,28 @@
 #include <gtk/gtk.h>
-#include <glade/glade.h>
 
-int main(int argc, char *argv[])
+void 
+on_window_destroy (GObject *object, gpointer user_data)
 {
-        gtk_init(&argc, &argv);
+        gtk_main_quit();
+}
 
-        GladeXML *xml = glade_xml_new("demo.glade", NULL, NULL);
+int
+main (int argc, char *argv[])
+{
+        GtkBuilder              *builder;
+        GtkWidget               *window;
+        
+        gtk_init (&argc, &argv);
+        
+        builder = gtk_builder_new ();
+        gtk_builder_add_from_file (builder, "calc.glade", NULL);
 
-        if (xml == NULL) {
-                g_print("glade_xml_new error\n");
-                return 1;
-        }
-
-        gtk_main();
-
+        window = GTK_WIDGET (gtk_builder_get_object (builder, "window"));
+        gtk_builder_connect_signals (builder, NULL);          
+        g_object_unref (G_OBJECT (builder));
+        
+        gtk_widget_show (window);       
+        gtk_main ();
+        
         return 0;
 }
