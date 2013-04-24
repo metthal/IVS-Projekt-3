8748c319735c0cf8c4d033f361a164d6e815bac1
Marek Milkovič <xmilko01@stud.fit.vutbr.cz>
2013-04-24 18:28:19 +0200

    Par zmien v matematickej kniznici

--- src/ml_math/ml_math.c
+++ src/ml_math/ml_math.c
@@ -1,49 +1,77 @@
 #include "ml_math.h"
 #include <math.h>
 
+int ml_math_err;
+
 double add(double a, double b)
 {
+    ml_math_err = ML_MATH_OK;
     return a+b;
 }
 
 double subtract(double a, double b)
 {
+    ml_math_err = ML_MATH_OK;
     return a-b;
 }
 
 double multiply(double a, double b)
 {
+    ml_math_err = ML_MATH_OK;
     return a*b;
 }
 
 double divide(double a, double b)
 {
+    if (b == 0.0)
+    {
+        ml_math_err = ML_MATH_ERROR_DIV_BY_ZERO;
+        return 0.0;
+    }
+
+    ml_math_err = ML_MATH_OK;
     return a/b;
 }
 
 double power(double x, double a)
 {
+    ml_math_err = ML_MATH_OK;
     return pow(x,a);
 }
 
 double root(double x, double a)
 {
+    ml_math_err = ML_MATH_OK;
     return pow(x,1/a);
 }
 
 double sinus(double x)
 {
+    ml_math_err = ML_MATH_OK;
     return sin(x);
 }
 
 double cosinus(double x)
 {
+    ml_math_err = ML_MATH_OK;
     return cos(x);
 }
 
 double factorial(double n)
 {
-    if (n <= 1)
+    if (n < 0)
+    {
+        ml_math_err = ML_MATH_ERROR_INVALID_INPUT;
+        return 0.0;
+    }
+
+    ml_math_err = ML_MATH_OK;
+    return _factorial(n);
+}
+
+double _factorial(double n)
+{
+    if (n <= 1.0)
         return 1.0;
 
     return n*factorial(n-1);
--- src/ml_math/ml_math.h
+++ src/ml_math/ml_math.h
@@ -1,3 +1,15 @@
+#ifndef ML_MATH_H
+#define ML_MATH_H
+
+enum Errors
+{
+    ML_MATH_OK                      = 0,
+    ML_MATH_ERROR_DIV_BY_ZERO,
+    ML_MATH_ERROR_INVALID_INPUT,
+};
+
+extern int ml_math_err;
+
 double add(double a, double b);
 double subtract(double a, double b);
 double multiply(double a, double b);
@@ -7,3 +19,6 @@ double root(double x, double a);
 double sinus(double x);
 double cosinus(double x);
 double factorial(double n);
+double _factorial(double n);
+
+#endif
